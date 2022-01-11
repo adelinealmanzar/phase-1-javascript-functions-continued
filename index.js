4af146f166e078034a65c2d5fa2fd30fc4112e50
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-11 15:31:38 -0800

    commit tree 274843da474cb54bf838ba18815e67ea6dbf1c53

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("go hiking");
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+saturdayFun("go hiking");
+
+function wrapAdjective(punctuation = "*") {
+    return function (adjective="special") {
+        return `You are ${punctuation}${adjective}${punctuation}!`;
+    }
+}
+
